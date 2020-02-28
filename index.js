b7431ef3e460f0bf69b5545aeb40b2f68ce5ce78
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-02-28 10:39:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(array, callback){
+    let r = [];
+    for (let i = 0; i < array.length; i++) {
+        r.push(callback(array[i]));
+    }
+    return r;
+}
+function reduce(array,callback, start = 0) {
+    let result = (!!start) ? start : array[0];
+    let i = (!!start) ? 0 : 1;
+    for (; i < array.length; i++) {
+        result = callback(array[i], result);
+    }
+    return result;
+}
